edb5b9f039f95d603021b14142c687a51599e232
Aziz <baziz1275@gmail.com>
2024-10-29 10:54:04 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//bir zholdyq tusinikteme
+
+console.log('salem javascript alemi');
+console.log(5*8);
+console.log(9-3);
+console.log("7 kosu 2 " + 4/2 + " javascript");
+/*
+kop zholdyq
+tusinikteme
+*/
+console.log('menin atym Aziz');
+console.log('bul syrtky bailanys');
